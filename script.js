941cb3bcac307fc92edcbc5d83bcac542c781341
WelcometoVisa <maxdiazg820@gmail.com>
2025-05-20 18:24:25 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function enviarWhatsApp() {
-    const nombre = document.getElementById('nombre').value;
-    const medio = document.getElementById('medio').value;
-    const mensaje = `Hola, soy ${nombre} y quiero ser contactado por ${medio}`;
-    window.open(`https://wa.me/573508840251?text=${encodeURIComponent(mensaje)}`, '_blank');
-}
