27b5e0cee07fe53773aec6505dc69ec24e440961
WelcometoVisa <maxdiazg820@gmail.com>
2025-05-20 17:11:39 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,11 +2,6 @@
 function enviarWhatsApp() {
     const nombre = document.getElementById('nombre').value;
     const medio = document.getElementById('medio').value;
-    const mensaje = document.getElementById('mensaje').value;
-
-    const texto = `Hola, mi nombre es ${nombre}. Deseo ser contactado por ${medio}. ${mensaje ? "Mensaje adicional: " + mensaje : ""}`;
-    const url = `https://wa.me/573508840251?text=${encodeURIComponent(texto)}`;
-
-    window.open(url, '_blank');
-    return false;
+    const mensaje = `Hola, soy ${nombre} y quiero ser contactado por ${medio}`;
+    window.open(`https://wa.me/573508840251?text=${encodeURIComponent(mensaje)}`, '_blank');
 }
